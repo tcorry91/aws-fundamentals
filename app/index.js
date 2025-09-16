bd91b37d36546b36848cc128a099664102e1fb40
tcorry91 <tcorry@hotmail.com.au>
2025-09-16 18:02:19 +1000

    CI: trigger build

--- app/index.js
+++ app/index.js
@@ -3,3 +3,5 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.get("/", (_req, res) => res.send("Hello from Dockerized Node!"));
 app.listen(port, "0.0.0.0", () => console.log(`Server listening on 0.0.0.0:${port}`));
+
+// trigger CI
