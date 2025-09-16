3c7312ae51c78e68537778b9382abbb3c385466b
tcorry91 <tcorry@hotmail.com.au>
2025-09-16 17:37:04 +1000

    adding index.js

---
+++ app/index.js
@@ -0,0 +1,5 @@
+﻿const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+app.get("/", (_req, res) => res.send("Hello from Dockerized Node!"));
+app.listen(port, "0.0.0.0", () => console.log(`Server listening on 0.0.0.0:${port}`));
