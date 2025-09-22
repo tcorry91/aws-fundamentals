6a2ef4731c92d8bf7454dca3f30c8c795c84b8cf
tcorry91 <tcorry@hotmail.com.au>
2025-09-22 14:59:28 +1000

    changed the index.js message

--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 ﻿const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-app.get("/", (_req, res) => res.send("Hello from Dockerized Node!"));
+app.get("/", (_req, res) => res.send("Hello from Dockerized Node! - vNext"));
 app.listen(port, "0.0.0.0", () => console.log(`Server listening on 0.0.0.0:${port}`));
-
-// trigger CI
